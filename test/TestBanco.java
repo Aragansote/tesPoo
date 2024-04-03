b58ad8e7730ec9367b3c30ababb3be2af424a881
Dabier <119388143+Aragansote@users.noreply.github.com>
2024-04-03 16:02:33 -0500

    Add files via upload

---
+++ test/TestBanco.java
@@ -0,0 +1,20 @@
+package test;
+
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTimeout;
+
+
+import org.junit.jupiter.api.Test;
+
+class TestBanco {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
